5a25601982402d8ef3533f01ec976742a7f8c0b6
Agboweroh <momohjacob20@gmail.com>
2026-02-09 14:54:12 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_hC78sDMDR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2p98rqr19f8dutemvr6gdd5l1b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
